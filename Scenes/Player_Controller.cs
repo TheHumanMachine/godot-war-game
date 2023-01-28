77ab249b68b4f5f334b03d2b5e4fcbfcd2171601
unknown <kristofer.koehn@wsu.edu>
2023-01-27 21:54:00 -0800

    added first person character entity, prototype map

---
+++ Scenes/Player_Controller.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+
+public partial class Player_Controller : CharacterBody3D
+{
+
+	private Node3D head;
+
+
+	public override void _Ready() {
+		head = GetNode<CollisionShape3D>("Head");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+	
+	public override void _UnhandledInput(InputEvent @event) {
+		if (@event is InputEventMouseMotion mouse) {
+			RotateY((float)(Math.PI / 180.0 * (-mouse.Relative.X * mouseSensitivity)));
+			head.RotateX((float)(Math.PI / 180.0 * (-mouse.Relative.Y * mouseSensitivity)));
+			head.Rotation = new Vector3((Math.Clamp(head.Rotation.X, (float)(Math.PI / 180.0 * -89),(float)(Math.PI / 180.0 * 89))), head.Rotation.Y, head.Rotation.Z);
+		}
+	}
+
+	public float mouseSensitivity = 0.06f;
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
